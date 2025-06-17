c1cf81792c67e9a42900cd28bf62fbe879e4ddbd
Yi Miao <40175039+yi-miao@users.noreply.github.com>
2025-06-17 17:58:57 -0400

    Update vcover.py
    use ffmpeg for better quality.

--- vcover.py
+++ vcover.py
@@ -1,29 +1,24 @@
 import cv2
 import os
 import time
-from mutagen.mp4 import MP4, MP4Cover
+import ffmpeg
+import subprocess
 
 class CoverSelector:
-    def __init__(self, video_path, cover_file):
+    def __init__(self, video_path, output_file, cover_file="selected.jpg"):
         self.video = video_path
+        self.output = output_file
         self.cover = cover_file
-
-    def set_mp4_cover(self, video_path, cover_path):
-        try:
-            video = MP4(video_path)
-            with open(cover_path, 'rb') as f:
-                cover_data = f.read()
-                
-            # Create MP4Cover object with the image data
-            cover = MP4Cover(cover_data, imageformat=MP4Cover.FORMAT_JPEG)        
-            # Set the cover art in the MP4 metadata
-            video['covr'] = [cover]
             
-            # Save the changes
-            video.save()
-            print(f"Successfully set cover for {video_path}")
-        except Exception as e:
-            print(f"Error: {e}")
+    def set_mp4_cover(self):
+        command = [
+            "ffmpeg", "-loglevel", "error", "-i", self.video, "-i", self.cover,
+            "-map", "0", "-map", "1",
+            "-c", "copy", "-disposition:v:1", "attached_pic",
+            self.output
+        ]
+        subprocess.run(command, check=True)
+        print(f"Successfully updated cover for {self.video} -> {self.output}")
         
     def run(self, num_frames = 20, beta = 5):
         window_name = "Select Cover"
@@ -32,18 +27,17 @@ class CoverSelector:
         self.cap = cv2.VideoCapture(self.video)
         frame_count = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
         frame_index = 0
-        print(f"Controls: Space = Pause/Resume, ← back {num_frames} frames, → forward {num_frames} frames, 's' select, 'q' quit")
         while True:
             self.cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
             ret, frame = self.cap.read()
             if frame is None:
-                print("End of video or invalid frame.")
+                self.cleanup()
                 break
             percent = (frame_index / frame_count) * 100
-            footer_text = "Frame " + str(frame_index) + " - " + str(round(percent, 1)) + " %" 
-            cv2.setWindowTitle(window_name, footer_text)
+            title_text = "Frame " + str(frame_index) + " - " + str(round(percent, 1)) + " %" 
+            cv2.setWindowTitle(window_name, title_text)
             cv2.imshow(window_name, frame)
-            key = cv2.waitKeyEx(0)
+            key = cv2.waitKeyEx(1)
             if key == ord('q'):     # quit
                 self.cleanup()
                 exit()
@@ -54,8 +48,8 @@ class CoverSelector:
                 self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
                 self.cleanup()
                 time.sleep(0.1)
-                self.set_mp4_cover(self.video, self.cover)
-                print(f"{self.video} cover replaced with {self.cover}")
+                self.set_mp4_cover()
+                # print(f"{self.video} cover replaced with {self.cover}")
                 exit()
             elif key == 2424832:  # Left arrow
                 frame_index = max(frame_index - num_frames, 0)
@@ -71,17 +65,13 @@ class CoverSelector:
         cv2.destroyAllWindows()
 
 if __name__ == "__main__":  
-    # usesage: python vcover.py -v "test.mp4" -c selected.jpg
+    # usesage: python vcover.py -v "video_in.mp4" -o "video_out.mp4"
     import argparse
 
     parser = argparse.ArgumentParser(description="Process video and cover file.")
     parser.add_argument("-v", "--video", required=True, help="Path to video file")
-    parser.add_argument("-c", "--cover", required=True, help="Path to cover file")
-    # Display results
-
+    parser.add_argument("-o", "--output", required=True, help="Path to output file")
     args = parser.parse_args()
-    print(f"Video file selected: {args.video}")
-    print(f"Cover file selected: {args.cover}")
-    
-    vcs = CoverSelector(args.video, args.cover)
-    vcs.run(60)
+  
+    app = CoverSelector(args.video, args.output)
+    app.run(60)
